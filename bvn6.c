e1bf0c97c7df9626dec4e02096502baf2f97b0e7
vulananh29112007-art <vulananh29112007@gmail.com>
2025-11-19 22:36:23 +0700

    Update bvn6.c

--- bvn6.c
+++ bvn6.c
@@ -1,51 +1,50 @@
 #include<stdio.h>
 #include<string.h>
-#define MAX 50
 
-struct Information {
+struct Student {
     char name[50];
     int age;
     float grade;
 };
 
 int main() {
-    struct Information sv[MAX];
+    struct Student student[5];
     int n = 5,i=0;
 
     for (int i = 0; i < n; i++) {
         
-        printf("Nhap ten SV %d: ", i+1);
-        fgets(sv[i].name, sizeof(sv[i].name), stdin);
-        sv[i].name[strcspn(sv[i].name, "\n")] = '\0';
+        printf("Nhap ten sv %d: ", i+1);
+        fgets(student[i].name, sizeof(student[i].name), stdin);
+        student[i].name[strcspn(student[i].name, "\n")] = '\0';
 
-        printf("Nhap tuoi SV %d: ", i+1);
-        scanf("%d", &sv[i].age);
+        printf("Nhap tuoi sv %d: ", i+1);
+        scanf("%d", &student[i].age);
         getchar();
 
         printf("Nhap diem trung binh: ");
-        scanf("%f",sv[i].grade);
+        scanf("%f",student[i].grade);
     }
     	printf("THONG TIN SINH VIEN\n");
 	for(int i = 0;i<n;i++){
-    	printf("Ten sv: %s\n",sv[i].name);
-    	printf("Tuoi sv: %d\n",sv[i].age);
-    	printf("Diem tb sv: %s\n",sv[i].grade);
+    	printf("Ten sv: %s\n",student[i].name);
+    	printf("Tuoi sv: %d\n",student[i].age);
+    	printf("Diem tb sv: %s\n",student[i].grade);
 	}
 
     int nameSearch, found = -1;
     printf("Nhap ten can tim kiem: ");
     scanf("%d", &nameSearch);
     for(int i = 0;i<n;i++){
-    	if(nameSearch == sv[i].name){
+    	if(nameSearch == student[i].name){
     		found = i;
 		}
 	}
     if(found == -1){
-    	printf("Khong tim thay sv\n");
+    	printf("Khong tim thay student\n");
 	}else{
-		printf("Ten sv: %s\n",sv[i].name);
-		printf("Tuoi sv: %d\n",sv[i].age);
-		printf("Diem tb sv: %f",sv[i].grade);
+		printf("Ten sv: %s\n",student[i].name);
+		printf("Tuoi sv: %d\n",student[i].age);
+		printf("Diem tb sv: %f",student[i].grade);
 		
 	}
 
@@ -53,4 +52,5 @@ int main() {
     return 0;
     
 }
+
 
